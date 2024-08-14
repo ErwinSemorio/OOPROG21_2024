9477546673876ec8aa2082789691616ad8f27fcb
wenexzcs <167678461+wenexzcs@users.noreply.github.com>
2024-08-14 13:40:59 +0000

    movie quote and tabe done

---
+++ Chapter 1/MovieQuoteInfor.java
@@ -0,0 +1,11 @@
+public class PrintCharacterPattern {
+    public static void main(String[] args) {
+        // Directly print each line of the pattern
+        System.out.println("Quote: Of course I'll hurt you. Of course you'll hurt me. Of course we will hurt each other But this us the very condition of existence. To become spring, means accepting the risk of winter. To become presence, means accepting the risk of absence. ");
+        System.out.println("Movie: The Little Prince");
+        System.out.println("Character: The Fox");
+        System.out.println("Year: 2015");
+        
+    }
+}
+
---
+++ Chapter 1/TableAndChairs.java
@@ -0,0 +1,11 @@
+public class PrintCharacterPattern {
+    public static void main(String[] args) {
+        // Directly print each line of the pattern
+        System.out.println("X                        X");
+        System.out.println("X                        X");
+        System.out.println("X                        X");
+        System.out.println("XXXXX  XXXXXXXXXXX  XXXXXX");
+        System.out.println("X   X  X         X  X    X");
+        System.out.println("X   X  X         X  X    X");
+    }
+}
