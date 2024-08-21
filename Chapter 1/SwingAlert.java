9c4a8fba6ce8badfdd8cdd46546157b321e32de4
ErwinSemorio <167678461+ErwinSemorio@users.noreply.github.com>
2024-08-21 13:05:11 +0000

    Swing Alert

---
+++ Chapter 1/SwingAlert.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class SwingAlert {
+    public static void main(String[] args) {
+        // Display a simple message dialog with a message
+        JOptionPane.showMessageDialog(null, "Hello, welcome to Swing!");
+    }
+}
+
