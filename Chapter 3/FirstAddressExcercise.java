91df6d11827a24fc82639b708dbe31920650b52b
ErwinSemorio <167678461+ErwinSemorio@users.noreply.github.com>
2024-10-04 07:53:41 +0800

    Create FirstAddressExcercise.java

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,18 @@
+public class FirstAddressExcercise {
+
+    // Method to display the Nokia HQ address
+    public static void displayNokiaAddress() {
+        System.out.println("Nokia Corporation");
+        System.out.println("Karakaari 7");
+        System.out.println("FI-02610 Espoo, Finland");
+        System.out.println("Company: Nokia");
+    }
+
+    // Main method to run the program
+    public static void main(String[] args) {
+        System.out.println("First Java application");
+        
+        // Call to the method that displays Nokia's address
+        displayNokiaAddress();
+    }
+}
