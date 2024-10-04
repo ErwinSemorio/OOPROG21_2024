67d1ce484c3f281a99e08dde071683bd81e7a206
ErwinSemorio <167678461+ErwinSemorio@users.noreply.github.com>
2024-10-04 22:47:27 +0800

    Update TestBloodData.java

--- Chapter 4/TestBloodData.java
+++ Chapter 4/TestBloodData.java
@@ -1,1 +1,11 @@
 
+public class TestBloodData {
+    public static void main(String[] args) {
+    
+        BloodData patient = new BloodData(); // Create a new patient
+
+        patient.inputBloodInfo(); // Input blood type and Rh factor
+
+        patient.displayBloodInfo(); // Display the entered information
+    }
+}
